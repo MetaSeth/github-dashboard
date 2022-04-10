113f75570d1552e7f4bcbdeb295e5821bafefa06
MetaSeth <alexandre.mokni@gmail.com>
2022-04-10 21:22:09 +0200

    repo details

--- src/app/shared/services/github.service.ts
+++ src/app/shared/services/github.service.ts
@@ -1,14 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {
-  BehaviorSubject,
-  Observable,
-  ReplaySubject,
-  Subject,
-  throwError,
-} from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-
+import { forkJoin, ReplaySubject } from 'rxjs';
 import { UserInfos } from '../models/user-infos.model';
 
 const URL = 'https://api.github.com';
@@ -17,13 +9,35 @@ const URL = 'https://api.github.com';
   providedIn: 'root',
 })
 export class GithubService {
+  private _userinfos = new ReplaySubject<UserInfos>(1);
+
   constructor(private http: HttpClient) {}
 
-  getUserInfos(username: string) {
-    return this.http.get(`${URL}/users/${username}`);
+  get userinfos() {
+    return this._userinfos.asObservable();
   }
-
-  getUserRepos(username: string) {
-    return this.http.get(`${URL}/users/${username}/repos`);
+  getUserInfosByUsername(username: string) {
+    forkJoin({
+      userInfo: this.http.get(`${URL}/users/${username}`),
+      userRepo: this.http.get(`${URL}/users/${username}/repos`),
+    }).subscribe((result: any) => {
+      this._userinfos.next({
+        username: result.userInfo['login'],
+        name: result.userInfo['name'],
+        avatarUrl: result.userInfo['avatar_url'],
+        repositorys: result.userRepo.reduce((acc: any, repo: any) => {
+          acc[repo.name] = {
+            name: repo.name,
+            language: repo.language,
+            stars: repo.stargazers_count,
+            description: repo.description,
+            creationDate: new Date(repo.created_at),
+            updateDate: new Date(repo.updated_at),
+            url: repo.html_url,
+          };
+          return acc;
+        }, {}),
+      });
+    });
   }
 }
